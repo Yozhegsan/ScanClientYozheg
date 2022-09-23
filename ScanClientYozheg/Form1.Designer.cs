f11691eaf01de0d03de2f3c8a65c258c5dba9c31
o.gnidin <o.gnidin@mhp.com.ua>
2022-09-23 09:32:39 +0300

    Туче

--- ScanClientYozheg/Form1.Designer.cs
+++ ScanClientYozheg/Form1.Designer.cs
@@ -31,17 +31,21 @@ namespace ScanClientYozheg
         {
             this.btnScan = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.txtIP = new System.Windows.Forms.TextBox();
+            this.txtPort = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // btnScan
             // 
             this.btnScan.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.btnScan.Location = new System.Drawing.Point(12, 12);
+            this.btnScan.Location = new System.Drawing.Point(12, 51);
             this.btnScan.Name = "btnScan";
-            this.btnScan.Size = new System.Drawing.Size(379, 123);
+            this.btnScan.Size = new System.Drawing.Size(368, 87);
             this.btnScan.TabIndex = 0;
-            this.btnScan.Text = "Scan now";
+            this.btnScan.Text = "Scan and save";
             this.btnScan.UseVisualStyleBackColor = true;
             this.btnScan.Click += new System.EventHandler(this.btnScan_Click);
             // 
@@ -49,18 +53,56 @@ namespace ScanClientYozheg
             // 
             this.pictureBox1.BackColor = System.Drawing.SystemColors.AppWorkspace;
             this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.pictureBox1.Location = new System.Drawing.Point(12, 141);
+            this.pictureBox1.Location = new System.Drawing.Point(12, 144);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(379, 432);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 1;
             this.pictureBox1.TabStop = false;
             // 
+            // txtIP
+            // 
+            this.txtIP.Location = new System.Drawing.Point(12, 25);
+            this.txtIP.Name = "txtIP";
+            this.txtIP.Size = new System.Drawing.Size(226, 20);
+            this.txtIP.TabIndex = 2;
+            this.txtIP.Text = "10.63.81.181";
+            // 
+            // txtPort
+            // 
+            this.txtPort.Location = new System.Drawing.Point(244, 25);
+            this.txtPort.Name = "txtPort";
+            this.txtPort.Size = new System.Drawing.Size(147, 20);
+            this.txtPort.TabIndex = 2;
+            this.txtPort.Text = "80";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Server IP";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(241, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Server Port";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(406, 586);
+            this.ClientSize = new System.Drawing.Size(402, 588);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtPort);
+            this.Controls.Add(this.txtIP);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.btnScan);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
@@ -72,6 +114,7 @@ namespace ScanClientYozheg
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -79,6 +122,10 @@ namespace ScanClientYozheg
 
         private System.Windows.Forms.Button btnScan;
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox txtIP;
+        private System.Windows.Forms.TextBox txtPort;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- ScanClientYozheg/Form1.cs
+++ ScanClientYozheg/Form1.cs
@@ -15,6 +15,10 @@ namespace ScanClientYozheg
 {
     public partial class Form1 : Form
     {
+        string ScanFolder = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "\\Scan";
+
+        //####################################################################################
+
         public Form1()
         {
             InitializeComponent();
@@ -22,7 +26,7 @@ namespace ScanClientYozheg
 
         private void Form1_Load(object sender, EventArgs e)
         {
-
+            if (!Directory.Exists(ScanFolder)) Directory.CreateDirectory(ScanFolder);
         }
 
         private void btnScan_Click(object sender, EventArgs e)
@@ -32,11 +36,12 @@ namespace ScanClientYozheg
 
         public void StartClient(byte[] cmd)
         {
-            byte[] bytes= new byte[1000000];
+            byte[] bytes = new byte[1000000];
             try
             {
-                IPAddress ipAddress = IPAddress.Parse("10.63.81.181");
-                IPEndPoint remoteEP = new IPEndPoint(ipAddress, 80);
+                //IPAddress ipAddress = IPAddress.Parse("10.63.81.181");
+                IPAddress ipAddress = IPAddress.Parse(txtIP.Text);
+                IPEndPoint remoteEP = new IPEndPoint(ipAddress, int.Parse(txtPort.Text));
                 Socket sender = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
                 try
                 {
@@ -46,6 +51,7 @@ namespace ScanClientYozheg
                     int bytesRec = sender.Receive(bytes);
                     MemoryStream ms = new MemoryStream(bytes);
                     Bitmap bmp = (Bitmap)Image.FromStream(ms);
+                    JpgToPdf(ref bmp);
                     pictureBox1.Image = bmp;
                     sender.Shutdown(SocketShutdown.Both);
                     sender.Close();
@@ -60,6 +66,21 @@ namespace ScanClientYozheg
                 MessageBox.Show(e.ToString());
             }
             GC.Collect();
+        }
+
+        private void JpgToPdf(ref Bitmap b)
+        {
+            string dt = DateTime.Now.ToString("yyyy.MM.dd_HH.mm.ss");
+            string pdf_fn = ScanFolder + "\\Scan(" + dt + ").pdf";
+            string bmp_fn = ScanFolder + "\\Scan(" + dt + ").jpg";
+            b.Save(bmp_fn);
+            iTextSharp.text.Image jpg = iTextSharp.text.Image.GetInstance(b, iTextSharp.text.BaseColor.WHITE);
+            var doc = new iTextSharp.text.Document(new iTextSharp.text.Rectangle(jpg.Width, jpg.Height), 0, 0, 0, 0);
+            iTextSharp.text.pdf.PdfWriter.GetInstance(doc, new FileStream(pdf_fn, FileMode.Create));
+            doc.Open();
+            jpg.Alignment = iTextSharp.text.Element.ALIGN_BASELINE;
+            doc.Add(jpg);
+            doc.Close();
         }
     }
 }
